92d55e13c145f037cd1be601d067f14e513e7840
Greg Dennis <gregsdennis@yahoo.com>
2020-11-22 16:36:18 +1300

    first try

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# dependencies-action
+# PR Dependency Check Action
+
 A GitHub Action that enforces PR dependencies
+
+## Example usage
+
+```yaml
+on: [pull_request]
+
+jobs:
+  check_dependencies:
+    runs-on: ubuntu-latest
+    name: check dependencies
+    steps:
+    - uses: gregsdennis/dependencies-action@master
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+getDependency = function(line) {
+    var rx = /^depends on #(\d*)$/gmi;
+    var match = rx.exec(line);
+    if (match !== null)
+        return parseInt(match[1], 10);
+    return null;
+};
+
+async function run() {
+    const myToken = process.env.GITHUB_TOKEN;
+    const octokit = github.getOctokit(myToken);
+
+    const { data: pullRequest } = await octokit.pulls.get({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: github.context.issue.number,
+    });
+
+    console.log(pullRequest.body);
+    const lines = pullRequest.body.split(/\r\n|\r|\n/);
+    
+    var dependencies = [];
+    lines.forEach(l => {
+        var dependency = getDependency(l);
+        if (dependency !== null)
+            dependencies += dependency;
+    });
+    console.log(dependencies);
+      
+    for (var d in dependencies) {
+        const { data: pr } = await octokit.pulls.get({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            pull_number: d,
+        });
+        if (!pr.merged && !pr.closed_at)
+            dependencyPullRequests += d
+    }
+
+    if (dependencyPullRequests.length !== 0) {
+        pullRequest.body.comments.create({
+            body: 'test'
+        });
+    }
+}
+
+try {
+    run();
+} catch (error) {
+    core.setFailed(error.message);
+}

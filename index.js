11cc148dcbfb2f2784f0f98e9acc3701f6ab61e0
Greg Dennis <gregsdennis@yahoo.com>
2023-08-24 13:56:55 +1200

    custom domain support

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
 
 const keyPhrases = 'depends on|blocked by';
-const issueTypes = 'issues|pull'
+const issueTypes = 'issues|pull';
+const domainsList = ['github.com', 'custom-domain.edu']; // add others from parameter
+const domainsString = combineDomains(domainsList);
+
 const quickLinkRegex = new RegExp(`(${keyPhrases}) #(\\d+)`, 'gmi');
 const partialLinkRegex = new RegExp(`(${keyPhrases}) ([-_\\w]+)\\/([-._a-z0-9]+)(#)(\\d+)`, 'gmi');
 const partialUrlRegex = new RegExp(`(${keyPhrases}) ([-_\\w]+)\\/([-._a-z0-9]+)\\/(${issueTypes})\\/(\\d+)`, 'gmi');
-const fullUrlRegex = new RegExp(`(${keyPhrases}) https:\\/\\/github\\.com\\/([-_\\w]+)\\/([-._a-z0-9]+)\\/(${issueTypes})\\/(\\d+)`, 'gmi');
-const markdownRegex = new RegExp(`(${keyPhrases}) \\[.*\\]\\(https:\\/\\/github\\.com\\/([-_\\w]+)\\/([-._a-z0-9]+)\\/(${issueTypes})\\/(\\d+)\\)`, 'gmi');
+const fullUrlRegex = new RegExp(`(${keyPhrases}) https?:\\/\\/(?:${domainsString})\\/([-_\\w]+)\\/([-._a-z0-9]+)\\/(${issueTypes})\\/(\\d+)`, 'gmi');
+const markdownRegex = new RegExp(`(${keyPhrases}) \\[.*\\]\\(https?:\\/\\/(?:${domainsString})\\/([-_\\w]+)\\/([-._a-z0-9]+)\\/(${issueTypes})\\/(\\d+)\\)`, 'gmi');
+
+function escapeDomainForRegex(domain) {
+    return domain.replace('.', '\\.');
+}
+
+function combineDomains(domains) {
+    return domains.map(x => escapeDomainForRegex(x)).join("|");
+}
+
+console.log(fullUrlRegex);
+
+return;
 
 function extractFromMatch(match) {
     return {

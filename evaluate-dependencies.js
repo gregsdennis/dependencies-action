5679e327803b06de7291d3b64be46239df41eb82
Bogusław Skrzypkowiak <skrzypkowiak@gmail.com>
2024-03-28 16:44:46 +0400

    x

--- evaluate-dependencies.js
+++ evaluate-dependencies.js
@@ -1,6 +1,6 @@
 import {Octokit} from "@octokit/core";
 import github from '@actions/github';
-import {getInput} from "@actions/core";
+import {getInput, info, error, setFailed, debug} from "@actions/core";
 
 var customDomains = getInput('custom-domains')?.split(/(\s+)/) ?? [];
 
@@ -37,7 +37,7 @@ function getAllDependencies(body) {
     var quickLinkMatches = [...body.matchAll(quickLinkRegex)];
     if (quickLinkMatches.length !== 0) {
         quickLinkMatches.forEach(match => {
-            core.info(`  Found number-referenced dependency in '${match}'`);
+            info(`  Found number-referenced dependency in '${match}'`);
             allMatches.push({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
@@ -52,7 +52,7 @@ function getAllDependencies(body) {
         .concat([...body.matchAll(markdownRegex)]);
     if (extractableMatches.length !== 0) {
         extractableMatches.forEach(match => {
-            core.info(`  Found number-referenced dependency in '${match}'`);
+            info(`  Found number-referenced dependency in '${match}'`);
             allMatches.push(extractFromMatch(match));
         });
     }
@@ -62,9 +62,9 @@ function getAllDependencies(body) {
 
 async function evaluate() {
     try {
-        core.info('Initializing...');
+        info('Initializing...');
         const myToken = process.env.GITHUB_AUTH || process.env.GITHUB_TOKEN;
-        core.info('Token acquired', myToken);
+        info('Token acquired', myToken);
         const octokit = new Octokit({
             auth: myToken
         });
@@ -76,28 +76,28 @@ async function evaluate() {
         });
 
         if (!pullRequest.body) {
-            core.info('body empty')
+            info('body empty')
             return;
         }
 
-        core.info('\nReading PR body...');
-        core.debug(JSON.stringify(pullRequest.body));
+        info('\nReading PR body...');
+        debug(JSON.stringify(pullRequest.body));
         var dependencies = getAllDependencies(pullRequest.body);
-        core.debug(JSON.stringify(dependencies));
-        core.info('\nAnalyzing lines y10...');
+        debug(JSON.stringify(dependencies));
+        info('\nAnalyzing lines y10...');
         var dependencyIssues = [];
         for (var d of dependencies) {
-            core.info(`  Fetching '${JSON.stringify(d)}'`);
+            info(`  Fetching '${JSON.stringify(d)}'`);
             var isPr = true;
 
             const r = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, {
                 owner: d.owner,
                 repo: d.repo,
                 pull_number: d.pull_number
-            }).catch(error => core.error(error));
-            core.info('RESPONSE:', JSON.stringify(r));
-            core.info('-------------------');
-            var response = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, d).catch(error => core.error(error));
+            }).catch(e => error(e));
+            info('RESPONSE:', JSON.stringify(r));
+            info('-------------------');
+            var response = await octokit.request(`GET /repos/{owner}/{repo}/pulls/{pull_number}`, d).catch(e => error(e));
             if (response === undefined) {
                 isPr = false;
                 d = {
@@ -105,10 +105,10 @@ async function evaluate() {
                     repo: d.repo,
                     issue_number: d.pull_number,
                 };
-                core.info(`  Fetching '${JSON.stringify(d)}'`);
-                response = await octokit.rest.issues.get(d).catch(error => core.error(error));
+                info(`  Fetching '${JSON.stringify(d)}'`);
+                response = await octokit.rest.issues.get(d).catch(error => error(error));
                 if (response === undefined) {
-                    core.info('    Could not locate this dependency.  Will need to verify manually.');
+                    info('    Could not locate this dependency.  Will need to verify manually.');
                     continue;
                 }
             }
@@ -116,19 +116,19 @@ async function evaluate() {
                 const {data: pr} = response;
                 if (!pr) continue;
                 if (!pr.merged && !pr.closed_at) {
-                    core.info('    PR is still open.');
+                    info('    PR is still open.');
                     dependencyIssues.push(pr);
                 } else {
-                    core.info('    PR has been closed.');
+                    info('    PR has been closed.');
                 }
             } else {
                 const {data: issue} = response;
                 if (!issue) continue;
                 if (!issue.closed_at) {
-                    core.info('    Issue is still open.');
+                    info('    Issue is still open.');
                     dependencyIssues.push(issue);
                 } else {
-                    core.info('    Issue has been closed.');
+                    info('    Issue has been closed.');
                 }
             }
         }
@@ -138,17 +138,14 @@ async function evaluate() {
             for (var pr of dependencyIssues) {
                 msg += `\n#${pr.number} - ${pr.title}`;
             }
-            core.setFailed(msg);
+            setFailed(msg);
         } else {
-            core.info("\nAll dependencies have been resolved!")
+            info("\nAll dependencies have been resolved!")
         }
-    } catch (error) {
-        core.setFailed(error.message);
+    } catch (e) {
+        setFailed(e.message);
         throw error;
     }
 }
 
-module.exports = {
-    evaluate: evaluate,
-    getAllDependencies: getAllDependencies
-}
+export {evaluate, getAllDependencies}
